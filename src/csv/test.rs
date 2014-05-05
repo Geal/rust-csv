fc9dd1acf62536c8ea7777d5ed8f5e4ca1c6da9a
Renato Zannon <renato@rrsz.com.br>
2014-05-05 19:29:57 -0300

    ~"str" literals are gone, use .to_owned() where appropriate

--- src/csv/test.rs
+++ src/csv/test.rs
@@ -7,37 +7,41 @@ mod lib;
 
 #[test]
 fn init() {
-  let s = ~"a,b,c,d";
+  let s = "a,b,c,d";
   let mut reader = ~BufReader::new(s.as_bytes());
   let mut p = csv::init(reader);
   let optrow = p.next();
-  assert_eq!(optrow.unwrap(), ~[~"a", ~"b", ~"c", ~"d"]);
+  assert_eq!(optrow.unwrap(), owned_string_vec(&["a", "b", "c", "d"]));
   assert!(p.next().is_none());
 }
 
 #[test]
 fn multiline() {
-  let s = ~"a,b,c,d\r\ne,f,g,h";
+  let s = "a,b,c,d\r\ne,f,g,h";
   let mut reader = ~BufReader::new(s.as_bytes());
   let mut p = csv::init(reader);
 
   let optrow = p.next();
-  assert_eq!(optrow.unwrap(), ~[~"a", ~"b", ~"c", ~"d"]);
+  assert_eq!(optrow.unwrap(), owned_string_vec(&["a", "b", "c", "d"]));
   let secoptrow = p.next();
-  assert_eq!(secoptrow.unwrap(), ~[~"e", ~"f", ~"g", ~"h"]);
+  assert_eq!(secoptrow.unwrap(), owned_string_vec(&["e", "f", "g", "h"]));
   assert!(p.next().is_none());
 }
 
 #[test]
 fn delim() {
-  let s = ~"aA ;b;c;d; e\r\nf;g;h; I;j\r\n";
+  let s = "aA ;b;c;d; e\r\nf;g;h; I;j\r\n";
   let mut reader = ~BufReader::new(s.as_bytes());
   let mut p = csv::init(reader);
   p.delim(';');
 
   let optrow = p.next();
-  assert_eq!(optrow.unwrap(), ~[~"aA ", ~"b", ~"c", ~"d", ~" e"]);
+  assert_eq!(optrow.unwrap(), owned_string_vec(&["aA ", "b", "c", "d", " e"]));
   let secoptrow = p.next();
-  assert_eq!(secoptrow.unwrap(), ~[~"f", ~"g", ~"h", ~" I", ~"j"]);
+  assert_eq!(secoptrow.unwrap(), owned_string_vec(&["f", "g", "h", " I", "j"]));
   assert!(p.next().is_none());
+}
+
+fn owned_string_vec(slice: &[&str]) -> Vec<~str> {
+  slice.iter().map(|string| string.to_owned()).collect::<Vec<~str>>()
 }
